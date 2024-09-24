a2ae415bb0feaa36e9d8e68e713e9a681561b893
Anon Dev <michaelerastus9@gmail.com>
2024-09-24 16:48:01 +0100

    Added screener

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -708,9 +708,31 @@
                                     </div>
                                 </div>
                                 <div class="uk-width-3-5@m uk-grid-margin">
-                                    <img class="in-img-mockup" src="{{asset('home/img/in-lazy.gif')}}"
-                                         data-src="{{asset('home/img/in-equity-12-mockup.png')}}"
-                                         alt="mockup" width="643" data-height="" data-uk-img="">
+                                    <!-- TradingView Widget BEGIN -->
+                                    <div class="tradingview-widget-container">
+                                        <div class="tradingview-widget-container__widget"></div>
+                                        <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+                                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js" async>
+                                            {
+                                                "exchanges": [],
+                                                "dataSource": "SPX500",
+                                                "grouping": "sector",
+                                                "blockSize": "market_cap_basic",
+                                                "blockColor": "change",
+                                                "locale": "en",
+                                                "symbolUrl": "",
+                                                "colorTheme": "dark",
+                                                "hasTopBar": false,
+                                                "isDataSetEnabled": false,
+                                                "isZoomEnabled": true,
+                                                "hasSymbolTooltip": true,
+                                                "isMonoSize": false,
+                                                "width": "100%",
+                                                "height": "100%"
+                                            }
+                                        </script>
+                                    </div>
+                                    <!-- TradingView Widget END -->
                                 </div>
                             </div>
                         </div>
